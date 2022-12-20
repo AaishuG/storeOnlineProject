5cc0d9e8e87fcd95e6c76012fe5be52f2e291d41
DELL <DELL@DESKTOP-42LRE2R>
2022-12-20 14:00:03 +0530

    storeOnlineProject

---
+++ src/main/java/storeOnline/storeOnline/App.java
@@ -0,0 +1,13 @@
+package storeOnline.storeOnline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
