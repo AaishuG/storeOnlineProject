6bd02cdebd36c077376c64e067803290018cf454
DELL <DELL@DESKTOP-42LRE2R>
2022-12-20 14:16:33 +0530

    update1

--- src/test/java/storeOnline/storeOnlineWithGit.java
+++ src/test/java/storeOnline/storeOnlineWithGit.java
@@ -3,8 +3,8 @@ package storeOnline;
 public class storeOnlineWithGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("storeOnline");
+System.out.println("storeOnline1");
 	}
 
 }
